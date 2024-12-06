9d27628b6b3f834333e8923915a8ded28e914189
Robert Todea <robert.todea@gmail.com>
2024-12-06 17:19:13 +0200

    Update configuration.nix

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -17,4 +17,7 @@
   networking.hostName = "nixos";
 
   system.stateVersion = "23.11";
+
+  nix.settings.trusted-substituters = [ "https://cache.flox.dev" ];
+  nix.settings.trusted-public-keys = [ "flox-cache-public-1:7F4OyH7ZCnFhcze3fJdfyXYLQw/aV7GEed86nQ7IsOs=" ];
 }
