90b8e8c31b3427b7051a7f1b7e64adc392c3fc52
Robert Todea <robert.todea@gmail.com>
2024-12-06 17:29:07 +0200

    Update configuration.nix

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -18,6 +18,7 @@
 
   system.stateVersion = "23.11";
 
+  nix.settings.experimental-features = ["nix-command" "flakes"];
   nix.settings.trusted-substituters = [ "https://cache.flox.dev" ];
   nix.settings.trusted-public-keys = [ "flox-cache-public-1:7F4OyH7ZCnFhcze3fJdfyXYLQw/aV7GEed86nQ7IsOs=" ];
 }
